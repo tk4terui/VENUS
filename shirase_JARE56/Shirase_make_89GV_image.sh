055f570e71f8747981f4fe946f527f02a662335c
tk4terui <6457848+tk4terui@users.noreply.github.com>
2020-04-13 15:05:01 +0900

    Add shirase dir and Delete old files

---
+++ shirase_JARE56/Shirase_make_89GV_image.sh
@@ -0,0 +1,70 @@
+idir0="/home/shirase/VENUS"
+ifile="${idir0}/%V/%Y%m/GW1AM2%Y%m%dA_110TB%V%P.dat"
+vars=("89GV" )
+ofile="/var/www/vishop/data/%Y%m/AM2SI%Y%m%d89GVM_%P.png"
+yymmdds=20141001
+yymmdde=100000000
+gmtscript="/home/shirase/VENUS/gmt4_make_89GV_image.sh"
+cd ${gmtscript%/*}
+script="./${gmtscript
+i=0
+while [ $i -lt ${
+  var=${vars[$i]}
+  yymms=`ls ${idir0}/${var}/ | head -1`
+  files=`ls ${idir0}/${var}/${yymms} | head -1`
+  tmps=`echo $files | sed -e "s/.*GW1AM2//"`
+  tmps=${tmps:0:8}
+  if [ ${tmps} -gt ${yymmdds} ]; then yymmdds=$tmps; fi
+  yymme=`ls ${idir0}/${var}/ | tail -1`
+  filee=`ls ${idir0}/${var}/${yymme} | tail -1`
+  tmpe=`echo $filee | sed -e "s/.*GW1AM2//"`
+  tmpe=${tmpe:0:8}
+  if [ ${tmpe} -lt ${yymmdde} ]; then yymmdde=$tmpe; fi
+  i=`expr $i + 1`
+done
+for pole in NP SP; do
+  yymmdd=$yymmdds
+  while [ $yymmdd -le $yymmdde ]; do
+    yy=${yymmdd:0:4}
+    mm=${yymmdd:4:2}
+    dd=${yymmdd:6:2}
+    flg0=1
+    flg1=0
+    for var in ${vars[@]}; do
+      file0=`echo $ifile | sed -e "s/%Y/$yy/g" \
+                               -e "s/%m/$mm/g" \
+                               -e "s/%d/$dd/g" \
+                               -e "s/%V/$var/g" \
+                               -e "s/%P/$pole/g"`
+      if [ ! -e $file0 ]; then
+        flg0=0
+      fi
+    done
+    if [ $flg0 -eq 1 ]; then
+      file1=`echo $ofile | sed -e "s/%Y/$yy/g" \
+                               -e "s/%m/$mm/g" \
+                               -e "s/%d/$dd/g" \
+                               -e "s/%P/$pole/g"`
+      if [ ! -e $file1 ]; then
+        flg1=1
+      else
+        for var in ${vars[@]}; do
+          file0=`echo $ifile | sed -e "s/%Y/$yy/g" \
+                                   -e "s/%m/$mm/g" \
+                                   -e "s/%d/$dd/g" \
+                                   -e "s/%V/$var/g" \
+                                   -e "s/%P/$pole/g"`
+          if [ $file1 -ot $file0 ]; then
+            flg1=1
+          fi
+        done
+      fi
+    fi
+    flg=$(( $flg0 * $flg1 ))
+    if [ $flg -eq 1 ]; then
+      ${script} $yymmdd$pole
+      mv -f ${file1
+    fi
+    yymmdd=`date -d"${yy}/${mm}/${dd} 1days" +%Y%m%d`
+  done
+done
---
+++ shirase_JARE56/VENUS/gmt4_make_89GV_image.sh
@@ -0,0 +1,29 @@
+tmpdir=/tmp
+for i in $@
+do
+./gpm2gmt $i 89GV
+echo $i
+TXT=$tmpdir/89GV_$i.txt
+EPS=$tmpdir/temp.eps
+PNG=$tmpdir/temp.png
+OUT=$tmpdir/output.png
+GMT gmtset ANNOT_FONT_SIZE_PRIMARY 6p
+GMT pscoast -R20/140/-75/-45 -Jm0.0569 -W0.1 -Gdarkgray -K > $EPS
+GMT psxy $TXT -R -J -Ss0.01  -C89G.cpt -K -O >> $EPS
+GMT psbasemap -R -J -Ba10g5 -K -O >> $EPS
+GMT pstext -R0/10/0/6 -Jx1 -K -O <<EOF >> $EPS
+0 4.5 8 0 0 BL AMSR2 89GV 
+EOF
+GMT pstext -R0/10/0/6 -Jx1 -O <<EOF >> $EPS
+7 4.5 8 0 0 BR Mercator 20E-140E for ${i:0:8}
+EOF
+GMT ps2raster $EPS -A -Tg -Qt 
+convert $PNG -rotate +90 \
+               -gravity north -splice 0x128 \
+               -gravity south -splice 0x100 \
+               NiPR-JAXA.logo.png -gravity southeast -composite \
+               ColorScale_89G.png -gravity southwest -composite \
+               -gravity south -splice 0x150 -alpha off $OUT
+mv $OUT AM2SI${i:0:8}89GVM_${i:8:2}.png
+rm $TXT $EPS $PNG
+done
